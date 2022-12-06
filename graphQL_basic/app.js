4e1a412aef6db086b154aeceefff6215d8c156df
TranVanSu2001 <tranvansu2001@gmail.com>
2022-12-06 23:04:43 +0700

    add .env

--- graphQL_basic/app.js
+++ graphQL_basic/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
 const app = express();
+require("dotenv").config();
 
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://tranvansu:Tranvansu661002@cluster0.em3dovv.mongodb.net/?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+// mongoose.connect(
+//   "mongodb+srv://tranvansu:Tranvansu661002@cluster0.em3dovv.mongodb.net/?retryWrites=true&w=majority",
+//   { useNewUrlParser: true }
+// );
+mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true });
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
